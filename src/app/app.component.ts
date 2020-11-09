cb6991b7ce6903ae849f9a47dc4f82e8540f1093
sabrina.mahjabin <mahjabin.yahoo.com>
2020-11-09 17:20:12 -0500

    Updating all the files

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from './question.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  started:boolean = false;
+}
